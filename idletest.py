7108082d9ebea37c02ebe5eddc9e96c63a5795d3
Matt Schmitt <mattrschmitt@gmail.com>
2013-11-05 14:44:47 -0600

    Testing git UI

---
+++ idletest.py
@@ -0,0 +1,25 @@
+Python 3.3.2 (v3.3.2:d047928ae3f6, May 16 2013, 00:06:53) [MSC v.1600 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = "Spam"
+>>> print(x)
+Spam
+>>> 
+[DEBUG ON]
+>>> 
+[DEBUG OFF]
+>>> ================================ RESTART ================================
+>>> print(x_
+      )
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    print(x_
+NameError: name 'x_' is not defined
+>>> print(x)
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    print(x)
+NameError: name 'x' is not defined
+>>> x = 8
+>>> print (x)
+8
+>>> 
